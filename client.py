268b8b1f744789440dca3af50012711e77e87214
Khoan-IT <duckhoan.ds@gmail.com>
2023-04-16 14:36:35 +0700

    delete model folder

--- client.py
+++ client.py
@@ -9,14 +9,14 @@ import intent_slot_service_pb2_grpc
 from underthesea import word_tokenize
 
 MONITOR_SERVER_INTERFACE = os.environ.get('HOST', 'localhost')
-MONITOR_SERVER_PORT = int(os.environ.get('PORT', 8000))
+MONITOR_SERVER_PORT = int(os.environ.get('PORT', 5002))
 
 CHANNEL_IP = f"{MONITOR_SERVER_INTERFACE}:{MONITOR_SERVER_PORT}"
 
 def main():
     channel = grpc.insecure_channel(CHANNEL_IP)
     stub = intent_slot_service_pb2_grpc.ISServiceStub(channel)
-    message = 'do khoa khoa học và kỹ thuật máy tính tổ chức á'
+    message = 'địa điểm tổ chức của sự kiện mùa hè xanh vào chiều mai lúc 8h20 21/10/2022'
     message = word_tokenize(message, format="text")
     result = stub.IntentSlotRecognize(intent_slot_service_pb2.IntentSlotRecognizeRequest(message=message))
     print(json.loads(result.message))
